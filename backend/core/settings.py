3912c46eb66cf3b0f83042acc1ad1ccb5120c610
Christo Joseph E <christojoseph.codes@gmail.com>
2026-08-15 18:36:18 +0530

    Fix DRF TokenAuthentication import path

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -104,7 +104,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django REST Framework Configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authtoken.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
